234b713a0b2e9be5d3013f5f5e5e51d43f40bec2
imaneroucdhi <imane.rcdi@gmail.com>
2023-01-05 17:30:56 +0100

    refactory code

--- info.php
+++ info.php
@@ -6,18 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="sass/main.css">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"></script> -->
 </head>
 <body class="info-page">
    
         
             <div class="progress-bar">
                 <div class="progress" id="progress"></div>
-                 <div class="progress-step step1 active" id="info"  >
-                   
-                 </div>
+                 <div class="progress-step step1 active" id="info"  ></div>
                  <!-- quiz -->
                  <div class="progress-step step2" id="quiz" >
                     <span style="color: #fff ; display: none;">quiz</span>
@@ -106,18 +101,16 @@
             
         </div>
         
-        
-        
-        
 
         <button class="btn-start"  id="btn-start-quiz" >Start Quiz</button>
             
 
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
-        
+
+
+
+
         <script src="asset/js/step-component.js"></script>
         <script src="asset/js/script.js"></script>
-
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
